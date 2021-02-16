5242bea8fca67a958823a83803c17b5a6f0cf9e9
suhail <78995086+suhail2021@users.noreply.github.com>
2021-02-16 10:43:30 +0530

    Add files via upload

---
+++ new1.py
@@ -0,0 +1,7 @@
+class Vehicle:
+    def __init__(self,max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+a=Vehicle("speed:60","milage:21")
+print(a.max_speed)
+print(a.mileage)
