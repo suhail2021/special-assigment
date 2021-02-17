0f9fad55d19fea6e98694b83f14c151d574d9cfa
suhail <78995086+suhail2021@users.noreply.github.com>
2021-02-17 10:55:59 +0530

    Add files via upload

---
+++ new16.py
@@ -0,0 +1,21 @@
+def evenodd(n):
+	if(n==0):
+		return 
+	elif(n==1):
+		return 
+	else:
+		return n-2		
+num=3,4,5,6,2,1
+if(evenodd(num)):
+	print(num,"num is even")
+else:
+	print(num,"num is odd")
+
+
+
+
+
+
+
+
+
